f3f789529c960e4266d17e04c7e77ba61c2c2c85
TestSerber <48034137+TestSerber@users.noreply.github.com>
2021-10-04 05:09:55 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 
-export const YARN_VERSION_ENDPOINT = "https://pastebin.com/raw/DKKtLQuq";
+export const YARN_VERSION_ENDPOINT = "https://raw.githubusercontent.com/TestSerber/minecraft-mapper/master/test.txt";
 
 export const YARN_JAR_URL = "https://maven.fabricmc.net/net/fabricmc/yarn/";
